1f3bb48f625c694398a9350583ef09433df9ccc3
Alec Lamson <lamsonak@s.dcsdk12.org>
2015-09-23 11:11:45 -0600

    Worked on q1

--- calculate.rb
+++ calculate.rb
@@ -1,8 +1,20 @@
 require_relative 'number_summary'
 
-a = (1..6).map { |i| i }
+a = (1..9).map { |i| i }
 print a, "\n"
 
-print NumberSummary.mode(a), "\n"
+print "Mode is " + NumberSummary.mode(a).to_s, "\n"
 
-puts NumberSummary.summarize('data.csv')
+print "Min is " + NumberSummary.min(a).to_s, "\n"
+
+print "Max is " + NumberSummary.max(a).to_s, "\n"
+
+#puts NumberSummary.summarize('data.csv')
+
+puts "q1 is " + NumberSummary.q1(a).to_s
+
+puts "q3 is " + NumberSummary.q3(a).to_s
+
+puts "median is " + NumberSummary.median(a).to_s
+
+puts "mean is " + NumberSummary.mean(a).to_s
