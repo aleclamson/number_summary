ec9d21d270de05e4485db7570ff1f233419d603b
Alec Lamson <lamsonak@s.dcsdk12.org>
2015-09-23 11:11:45 -0600

    Worked on q1

--- number_summary.rb
+++ number_summary.rb
@@ -1,9 +1,63 @@
-# @author Tim Padjen <tpadjen@gmail.com>
+# @author Alec Lamson <lamsonak@s.dcsdk12.org>
+# @author Henry Kaufman <hkaufman19@gmail.com>
 class NumberSummary
 	
 	class << self
 
-		# code goes here
+		# file = File.open("data.csv", "r")
+		# data = file.read
+
+		# array = data.split(",")
+		
+		def max(array)
+			array.max
+		end
+
+		def min(array)
+			array.min
+		end
+
+		def mean(array)
+			sum = 0.0 
+			for i in 1..array.length
+				sum += array[i-1].to_f
+			end
+			(sum / array.length)
+		end
+
+		def median(array)
+			if array.length % 2 == 0
+				(array[array.length / 2].to_f + array[(array.length / 2) - 1].to_f) / 2
+			else
+				array[array.length / 2]
+			end
+		end
+
+		def mode(array)
+
+		end
+
+		def q1(array)
+			if array.length % 2 == 0
+				new_arr = array[0..(array.length/2) - 1]
+				if new_arr.length % 2 == 0
+					(new_arr[new_arr.length / 2].to_f + new_arr[(new_arr.length / 2) - 1].to_f) / 2
+				else
+					new_arr[((new_arr.length + 1) / 2) - 1]
+				end
+			else
+				new_arr = array[0..((array.length + 1) / 2) - 2]
+				if new_arr.length % 2 == 0
+					(new_arr[new_arr.length / 2].to_f + new_arr[(new_arr.length / 2) - 1].to_f) / 2
+				else
+					new_arr[((new_arr.length + 1) / 2) - 1]
+				end
+			end
+		end
+
+		def q3(array)
+			
+		end
 
 	end
 
