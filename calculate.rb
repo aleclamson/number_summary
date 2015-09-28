fbd369bbff33d987c1a8d89d4b915c94d319cb4f
Alec Lamson <lamsonak@s.dcsdk12.org>
2015-09-28 10:20:31 -0600

    sigma

--- calculate.rb
+++ calculate.rb
@@ -1,7 +1,12 @@
 require_relative 'number_summary'
 
-a = (1..7).map { |i| i }
-print a, "\n"
+# a = (1..7).map { |i| i }
+# print a, "\n"
+
+file = File.open("data.csv", "r")
+data = file.read
+
+a = data.split(",")
 
 print "Mode is " + NumberSummary.mode(a).to_s, "\n"
 
@@ -18,5 +23,7 @@ puts "q3 is " + NumberSummary.q3(a).to_s
 puts "median is " + NumberSummary.median(a).to_s
 
 puts "mean is " + NumberSummary.mean(a).to_s
+
+puts "sigma is " + NumberSummary.sigma(a).to_s
 
 # 1,2,3,4,5,6,7
