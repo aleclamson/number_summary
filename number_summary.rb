453ef08ab101ead9912867cefa5590f21436b6c1
Alec Lamson <lamsonak@s.dcsdk12.org>
2015-09-29 10:05:53 -0600

    mode works

--- number_summary.rb
+++ number_summary.rb
@@ -21,18 +21,40 @@ class NumberSummary
 		end
 
 		def median(array)
-			if array.length % 2 == 0
-				(array[array.length / 2].to_f + array[(array.length / 2) - 1].to_f) / 2
+			a = array.sort
+			if a.length % 2 == 0
+				(a[a.length / 2].to_f + a[(a.length / 2) - 1].to_f) / 2
 			else
-				array[array.length / 2]
+				a[a.length / 2].to_f
 			end
 		end
 
 		def mode(array)
-
+			max = 0
+			array_uni = array.sort.uniq
+			array_count = []
+			array_final = []
+			(0...array_uni.length).each do |n|
+				array_count.push(0)
+			end
+			(0...array.length).each do |n|
+				(0...array_uni.length).each do |x|
+					if array_uni[x] == array[n]
+						array_count[x] += 1
+					end
+				end
+			end
+			max = array_count.max
+			(0...array_count.length).each do |n|
+				if array_count[n] == max
+					array_final.push(array_uni[n])
+				end
+			end
+			array_final
 		end
 
-		def q1(array)
+		def q1(a)
+			array = a.sort
 			if array.length % 2 == 0
 				new_arr = array[0..(array.length/2) - 1]
 				if new_arr.length % 2 == 0
@@ -50,7 +72,8 @@ class NumberSummary
 			end
 		end
 
-		def q3(array)
+		def q3(a)
+			array = a.sort
 			if array.length % 2 == 0
 				new_arr = array[(array.length/2)..array.length]
 				if new_arr.length % 2 == 0
@@ -78,7 +101,24 @@ class NumberSummary
 			for i in 1..newArray.length
 				newSum += newArray[i-1].to_f
 			end
-			Math.sqrt(newSum / array.length)
+			Math.sqrt(newSum / array.length).to_f
+		end
+
+		def summarize(file)
+			file = File.open(file, "r")
+			data = file.read
+
+			a = data.split(",")
+			
+			puts "Number Summary:"
+			puts "min: " + min(a).to_s
+			puts "max: " + max(a).to_s
+			puts "mean: " + mean(a).to_s
+			puts "median: " + median(a).to_s
+			puts "q1: " + q1(a).to_s
+			puts "q3: " + q3(a).to_s
+			puts "mode: " + mode(a).to_s
+			puts "sigma: " + sigma(a).to_s
 		end
 
 	end
